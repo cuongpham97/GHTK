fccde0b00973b1f35cf6faf74ac87eb69158f4ed
cuongpham97 <phamquoccuong197@gmail.com>
2022-05-30 09:56:52 +0700

    fix: format code

--- Lay_ma_don_quan_li_thu_tien.js
+++ Lay_ma_don_quan_li_thu_tien.js
@@ -1,12 +1,14 @@
+const obj = {
+    'Đang giao hàng': 'not-reviewed-packages',
+    'Đã giao hàng': 'delivered-packages-by-operator',
+    'Đã giao một phần': 'delivered-apart-packages-by-cod',
+    'Hủy trả shop': 'not-delivered-packages-by-operator',
+    'Delay': 'delay-delivering-packages-by-operator'
+};
+
 console.clear();
-const delivering = document.getElementById('not-reviewed-packages').querySelectorAll(".aui-lozenge>H5>a");
-console.log(`Đang giao hàng: ${delivering .length}:`);
-if (delivering.length) console.log([...delivering].map(a => a.textContent.match(/\d{10}/)[0]).join(" "));
 
-const delay = document.getElementById('delay-delivering-packages-by-operator').querySelectorAll(".aui-lozenge>H5>a");
-console.log(`Delay ${delay.length}:`);
-if (delay.length) console.log([...delay].map(a => a.textContent.match(/\d{10}/)[0]).join(" "));
-
-const delivered = document.getElementById('delivered-packages-by-operator').querySelectorAll(".aui-lozenge>H5>a");
-console.log(`Delivered ${delivered.length}:`);
-if (delivered.length) console.log([...delivered].map(a => a.textContent.match(/\d{10}/)[0]).join(" "));
+for ([status, className] of Object.entries(obj)) {
+    let elems = document.getElementById(className).querySelectorAll(".aui-lozenge>H5>a");
+    console.log(`${status} ${elems.length}: ` + [...elems].map(a => a.textContent.match(/\d{10}/)[0]).join(" "));
+}
